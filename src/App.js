a7d68be4f53e720b2d1778104f0038cdb65346fd
Stephen <omalleypatstephen@gmail.com>
2021-10-14 14:21:33 +0100

    CloudComputingWeek3

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Todo from "./Components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Tools</h1>
+      <Todo text='Learn React'/>
+      <Todo text='Master React'/>
+    </div>
+  );
+}
+
+export default App;
