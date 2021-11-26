1c5816f388357c291e0c307d918a46d3482494c2
Stephen <omalleypatstephen@gmail.com>
2021-11-26 14:02:05 +0000

    Cloud Computing Project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
-import FavoritesPage from './pages/Favorites';
+
 import Layout from './components/layout/Layout';
 
 function App() {
@@ -14,9 +14,6 @@ function App() {
         </Route>
         <Route path='/new-meetup'>
           <NewMeetupPage />
-        </Route>
-        <Route path='/favorites'>
-          <FavoritesPage />
         </Route>
       </Switch>
     </Layout>
